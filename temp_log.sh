9fe50920a425b31d85975b9c3dcc6f8d807cf53b
ANKIT RAJ <araj7540@gmail.com>
2017-06-10 14:08:02 +0800

    Create temp_log.sh

--- temp_log.sh
+++ temp_log.sh
@@ -2,6 +2,5 @@
 # Log temperature over some time using this script run as cron job.
 # Set your interval given as days, hours, minutes or seconds as the cron job itself.
 #----------------------------------------------------------------------
-#output will be a txt file `t_log.txt` which will have n columns for n cores and day and time.
-
-        (sensors | grep -A 0 'Core' | cut -c18-21 |tr "\n" "\t"; echo  $(date +"%m/%d/%Y %T") )>> temp_log.txt
+#output will be a txt file `temp_log.txt` which will have n columns for n cores and day and time.
+(sensors | grep -A 0 'Core' | cut -c18-21 |tr "\n" "\t"; echo  $(date +"%m/%d/%Y %T") )>> temp_log.txt
