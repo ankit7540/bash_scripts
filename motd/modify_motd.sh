1ca5603ea4fd537674e63f84665269429650af38
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2020-12-22 14:19:53 +0800

    removing additional messages

--- motd/modify_motd.sh
+++ motd/modify_motd.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 # Change the MOTD in Ubuntu
-
+# ---------------------------------------------
 echo -e "\tChanging MOTD"
 sudo chmod -x /etc/update-motd.d/10-help-text;
 sudo chmod -x /etc/update-motd.d/50-motd-news;
 sudo chmod -x /etc/update-motd.d/80-esm;
 sudo chmod -x /etc/update-motd.d/80-livepatch;
+
+sudo chmod -x /etc/update-motd.d/91-release-upgrade;
+sudo chmod -x /etc/update-motd.d/92-unattended-upgrades;
+sudo chmod -x /etc/update-motd.d/95-hwe-eol;
+
+# ---------------------------------------------
 
 sudo cat header_template.sh > /etc/update-motd.d/00-header;
 
